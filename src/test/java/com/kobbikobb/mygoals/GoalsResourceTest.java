12a1d981f7e41de873240985454f48f383ba31f0
Jakob Jónasson <jakob.jonasson@gmail.com>
2017-10-08 17:30:06 +0000

    Change tests to use rest-assured

--- src/test/java/com/kobbikobb/mygoals/GoalsResourceTest.java
+++ src/test/java/com/kobbikobb/mygoals/GoalsResourceTest.java
@@ -1,35 +1,24 @@
 package com.kobbikobb.mygoals;
 
-import javax.ws.rs.client.Client;
-import javax.ws.rs.client.ClientBuilder;
-import javax.ws.rs.client.WebTarget;
-
+import io.restassured.RestAssured;
 import org.glassfish.grizzly.http.server.HttpServer;
 
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
-import static org.junit.Assert.assertEquals;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.containsString;
 
 public class GoalsResourceTest {
 
     private HttpServer server;
-    private WebTarget target;
 
     @Before
     public void setUp() throws Exception {
-        // start the server
         server = Main.startServer();
-        // create the client
-        Client c = ClientBuilder.newClient();
-
-        // uncomment the following line if you want to enable
-        // support for JSON in the client (you also have to uncomment
-        // dependency on jersey-media-json module in pom.xml and Main.startServer())
-        // --
-        // c.configuration().enable(new org.glassfish.jersey.media.json.JsonJaxbFeature());
-
-        target = c.target(Main.BASE_URI);
+        RestAssured.baseURI = Main.BASE_URI;
+        RestAssured.enableLoggingOfRequestAndResponseIfValidationFails();
     }
 
     @After
@@ -37,12 +26,13 @@ public class GoalsResourceTest {
         server.stop();
     }
 
-    /**
-     * Test to see that the message "Got it!" is sent in the response.
-     */
     @Test
     public void shouldGetGoals() {
-        String responseMsg = target.path("goals").request().get(String.class);
-        assertEquals("Got it!", responseMsg);
+        given()
+                .when()
+                .get( "/goals")
+                .then()
+                .statusCode(200)
+                .body("[0].description", containsString("Learn how to relax with my children"));
     }
 }
